28309a034b9650ba744be7f6803acf226a720f3c
juan.sanchez <juan.sanchez@borderfree.com>
2014-07-23 12:49:21 -0400

    Refactored test and usage test

--- tests/PHPBenchtime.test.php
+++ tests/PHPBenchtime.test.php
@@ -1,37 +1,37 @@
 <?php
 
-include "PHPBenchTime.php";
+class Timer_Test extends PHPUnit_Framework_TestCase {
+    private $fetch;
 
-class Timer_Test extends PHPUnit_Framework_TestCase{
-    public function setUp(){
+    public function setUp() {
         $this->fetch = new PHPBenchTime\Timer;
     }
-    
-	public function testClassAttributes(){
-		$this->assertClassHasAttribute("starTime", "PHPBenchTime\Timer");
-		$this->assertClassHasAttribute("pauseTime", "PHPBenchTime\Timer");
-		$this->assertClassHasAttribute("endTime", "PHPBenchTime\Timer");
-		$this->assertClassHasAttribute("phpVersion", "PHPBenchTime\Timer");
-		$this->assertClassHasAttribute("lapName", "PHPBenchTime\Timer");
-	}
 
-	public function testStartFunctionNoName(){
-		$this->assertTrue($this->fetch->Start());
-		$this->assertArrayHasKey(0, $this->fetch->_startTime);
-		$this->assertEmpty($this->fetch->_lapName);
+    public function testClassAttributes() {
+        $this->assertClassHasAttribute( 'starTime', 'PHPBenchTime\Timer' );
+        $this->assertClassHasAttribute( 'pauseTime', 'PHPBenchTime\Timer' );
+        $this->assertClassHasAttribute( 'endTime', 'PHPBenchTime\Timer' );
+        $this->assertClassHasAttribute( 'phpVersion', 'PHPBenchTime\Timer' );
+        $this->assertClassHasAttribute( 'lapName', 'PHPBenchTime\Timer' );
+    }
 
-		$this->assertTrue($this->fetch->Start("Lapname"));
-		$this->assertArrayHasKey(0, $this->fetch->_startTime);
-		$this->assertNotNull($this->fetch->_lapName);
-	}
+    public function testStartFunctionNoName() {
+        $this->assertTrue( $this->fetch->Start() );
+        $this->assertArrayHasKey( 0, $this->fetch->_startTime );
+        $this->assertEmpty( $this->fetch->_lapName );
 
-	public function testLap(){
-		$mockLap = $this->getMock('PHPBenchTime\Timer');
-		$mockLap->expects($this->any())->method("Lap")->will($this->returnValue(null));
-	}
+        $this->assertTrue( $this->fetch->Start( 'Lapname' ) );
+        $this->assertArrayHasKey( 0, $this->fetch->_startTime );
+        $this->assertNotNull( $this->fetch->_lapName );
+    }
 
-	public function testEnd(){
-		$this->assertArrayHasKey("Start", $this->fetch->End());
-		$this->assertArrayHasKey("End", $this->fetch->End());
-	}
+    public function testLap() {
+        $mockLap = $this->getMock( 'PHPBenchTime\Timer' );
+        $mockLap->expects( $this->any() )->method( 'Lap' )->will( $this->returnValue( null ) );
+    }
+
+    public function testEnd() {
+        $this->assertArrayHasKey( 'Start', $this->fetch->End() );
+        $this->assertArrayHasKey( 'End', $this->fetch->End() );
+    }
 }
